1f096b4a7d0dae66a2c113d1f31f4f820f75c18f
Roberto Piu <60385159+roberto20007113@users.noreply.github.com>
2020-03-07 15:40:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function add() {
         var ul = document.getElementById("lista");
         var candidate = document.getElementById("input");
         var li = document.createElement("li");
-        li.setAttribute('id', candidate.value);
+        li.setAttribute("id", candidate.value);
         li.appendChild(document.createTextNode(candidate.value));
         ul.appendChild(li);
     }
